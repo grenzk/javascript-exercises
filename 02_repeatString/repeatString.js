9b305f2fe86e02da3a0402248f4ffc3c29fcb563
grenzk <74033991+grenzk@users.noreply.github.com>
2022-02-01 17:11:29 +0800

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, count) {
+  if (count < 0) {
+    return "ERROR";
+  } else {
+    return word.repeat(count);
+  }
 };
 
 // Do not edit below this line
