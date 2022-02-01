7c204b37f77fdc393bffc0bc967c1f80e7fc13a8
grenzk <74033991+grenzk@users.noreply.github.com>
2022-02-01 21:04:17 +0800

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...items) {
+  let index;
+  for (let i = 0; i < items.length; i++) {
+    if (array.includes(items[i])) {
+      index = array.indexOf(items[i]);
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
