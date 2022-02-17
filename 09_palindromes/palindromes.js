d62601ba2cbbff44e7627566ae6d565e7941c243
grenzk <74033991+grenzk@users.noreply.github.com>
2022-02-17 20:10:26 +0800

    Add my solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  return isPalindrome(words);
 };
 
+function isPalindrome(string) {
+  const newString = string
+    .toUpperCase()
+    .replace(/[^A-Z]/g, "");
+  const finalString = newString
+    .split("")
+    .reverse()
+    .join("");
+  return newString === finalString;
+}
 // Do not edit below this line
 module.exports = palindromes;
