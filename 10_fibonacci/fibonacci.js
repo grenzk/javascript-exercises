88098cca27b6038988cfe83df79623b1983a6b19
grenzk <74033991+grenzk@users.noreply.github.com>
2022-02-18 17:50:05 +0800

    Add my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (fibNum) {
+  const arr = [];
+  let num = 1;
+  arr[0] = num;
+  let i = 0;
 
+  parseInt(fibNum);
+  if (fibNum > 0) {
+    while (i < fibNum - 1) {
+      if (i === 0) {
+        num = 0 + arr[0];
+      } else {
+        num = num + arr[i - 1];
+      }
+      i++;
+      arr[i] = num;
+    }
+    return arr[fibNum - 1];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
